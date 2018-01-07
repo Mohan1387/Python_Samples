442cb050e5eabb4fe0620a090b05d04385e0e9cb
Mohan1387 <30650805+Mohan1387@users.noreply.github.com>
2018-01-07 08:13:48 +0000

    Update getdomaindate.py
    re formated

--- getdomaindate.py
+++ getdomaindate.py
@@ -4,20 +4,20 @@ import json
 import sys
 import datetime
 
-for line in sys.stdin:    
- line = line.strip()    
- req = url.Request("http://api.bulkwhoisapi.com/whoisAPI.php?domain="+line+"&token=usemeforfree")    
- response = url.urlopen(req)    
- the_page = response.read()    
- if(the_page != 'Invalid Input!'):        
- res_dict = json.loads(the_page)        
- if(res_dict['response_code'] != "failure"):            
- creation_date = res_dict['formatted_data']['CreationDate']            
- #print(creation_date[:10].encode('utf8'))            
- sys.stdout.write(creation_date[:10].encode('utf8'))        
- else:            
- #print("9999-01-01")            
- sys.stdout.write("9999-01-01")    
- else:        
- #print("9999-01-01")        
- sys.stdout.write("9999-01-01")
+for line in sys.stdin:
+ line = line.strip()
+ req = url.Request("http://api.bulkwhoisapi.com/whoisAPI.php?domain="+line+"&token=usemeforfree")
+ response = url.urlopen(req)
+ the_page = response.read()
+ if(the_page != 'Invalid Input!'):
+  res_dict = json.loads(the_page)
+  if(res_dict['response_code'] != "failure"):
+   creation_date = res_dict['formatted_data']['CreationDate']
+   #print(creation_date[:10].encode('utf8'))
+   sys.stdout.write(creation_date[:10].encode('utf8'))
+  else:
+   #print("9999-01-01")
+   sys.stdout.write("9999-01-01")    
+ else:
+  #print("9999-01-01")
+  sys.stdout.write("9999-01-01")
